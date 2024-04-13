442eb28e60840ca316a8b92a0e16093105763148
yash <yashwanthvs@gmail.com>
2024-04-13 11:58:54 +0530

    initialize vite react-ts project

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+      </div>
+      <h1>Vite + React + TS</h1>
+    </>
+  )
+}
+
+export default App
